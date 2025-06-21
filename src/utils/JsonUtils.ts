d29d99237b2b36028c4fa192ae3191e5e309408a
ARIKBOGA <barikboga42@gmail.com>
2025-06-21 17:25:09 +0300

    Refactor JSON file reading in JsonUtils.ts for improved clarity and efficiency

--- src/utils/JsonUtils.ts
+++ src/utils/JsonUtils.ts
@@ -7,15 +7,15 @@ export function mergeJsons(...jsons: any[]): any[] {
 
 function main() {
   const jsonsFolder = path.resolve(__dirname, "../output");
-  const jsonFiles = fs
+  const jsons = fs
     .readdirSync(jsonsFolder)
     .filter((file) => file.endsWith(".json") && file.includes("TRW"))
-    .sort((a, b) => a.localeCompare(b));
+    .sort((a, b) => a.localeCompare(b))
+    .map((file) => {
+      const filePath = path.join(jsonsFolder, file);
+      return JSON.parse(fs.readFileSync(filePath, "utf8"));
+    });
 
-  const jsons = jsonFiles.map((file) => {
-    const filePath = path.join(jsonsFolder, file);
-    return JSON.parse(fs.readFileSync(filePath, "utf8"));
-  });
   const mergedJson = mergeJsons(...jsons);
   const outputFilePath = path.join(jsonsFolder, "merged_TRW.json");
   fs.writeFileSync(outputFilePath, JSON.stringify(mergedJson, null, 2), "utf8");
