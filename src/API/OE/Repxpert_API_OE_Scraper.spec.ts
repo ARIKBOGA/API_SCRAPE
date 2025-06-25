f85b7be607376ecb02809a556d0d97a7e8161a99
ARIKBOGA <you@example.com>
2025-06-25 08:42:10 +0300

    feat: Add cross numbers processing and update vehicle compatibility output file naming

--- src/API/OE/Repxpert_API_OE_Scraper.spec.ts
+++ src/API/OE/Repxpert_API_OE_Scraper.spec.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import fs from "fs";
 import { readProductReferencesFromExcel } from "../../utils/Excel_Utils";
-import { processProductFor_OE, processProductFor_VehicleCompatibility } from "../../utils/api-workers";
+import { processProductFor_CrossNumbers, processProductFor_OE, processProductFor_VehicleCompatibility } from "../../utils/api-workers";
 import { referenceArray } from "../../utils/Types";
 
 dotenv.config({ path: path.resolve(".env") });
@@ -38,7 +38,7 @@ test("Get OE numbers for all products", async () => {
 test("Get Vehicle Compatibility for all products", async () => {
   test.setTimeout(20 * 60 * 1000);
   console.log(`Processing Vehicle Compatibility for brand: ${filterBrand}`);
-  const productReferences = referenceArray //readProductReferencesFromExcel();
+  const productReferences = readProductReferencesFromExcel();
 
   const { default: pLimit } = await import("p-limit");
   const limit = pLimit(3); // aynı anda n request sınırı
@@ -53,6 +53,25 @@ test("Get Vehicle Compatibility for all products", async () => {
 
   const results = (await Promise.all(promises)).filter((r) => r !== null);
 
-  const outputFilePath = path.resolve(`src/output/${productType}/Vehicle-Compatibility_${filterBrand}_6.json`);
+  const outputFilePath = path.resolve(`src/output/${productType}/Vehicle-Compatibility_${filterBrand}_ADD.json`);
   fs.writeFileSync(outputFilePath, JSON.stringify(results, null, 2), "utf8");
 });
+
+test("Get cross numbers via given cross/OE numbers", async () => {
+  test.setTimeout(20 * 60 * 1000);
+  console.log(`Processing Vehicle Compatibility for brand: ${filterBrand}`);
+  const productReferences = readProductReferencesFromExcel();
+
+  const { default: pLimit } = await import("p-limit");
+  const limit = pLimit(3); // aynı anda n request sınırı
+  const promises = productReferences
+  .filter((ref) => ref.brand.toLowerCase() === filterBrand.toLowerCase())
+  //.slice(0,10) // aralıkları 100, 200 gibi yap
+  .map((productRef) => limit(() => processProductFor_CrossNumbers(productRef)));
+
+  const results = (await Promise.all(promises)).filter((r) => r !== null);
+
+  const outputFilePath = path.resolve(`src/output/${productType}/Cross-Numbers_2.json`);
+  fs.writeFileSync(outputFilePath, JSON.stringify(results, null, 2), "utf8");
+
+})
