f90500970e47dd27b25f855f0bbb051af95ad13e
ARIKBOGA <you@example.com>
2025-06-26 12:26:07 +0300

    fix: Increase query size for cross number API requests from 100 to 200

--- src/utils/api-workers.ts
+++ src/utils/api-workers.ts
@@ -171,7 +171,7 @@ export async function processProductFor_CrossNumbers(element: ProductReference){
   const crossNumberURL_1 = process.env.CROSS_NUMBER_URL_1 || "";
   const crossNumberURL_2 = process.env.CROSS_NUMBER_URL_2 || "";
   const queryNumber = element.crossNumber;
-  const querySize = "100";
+  const querySize = "200";
 
   const crossNumberURL = `${crossNumberURL_1}${queryNumber}${crossNumberURL_2}${querySize}`;
   //console.log(crossNumberURL);
