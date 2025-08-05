4d5a9243fcf4145c4367a68dc005eeffb806cb06
ARIKBOGA <you@example.com>
2025-08-05 18:01:33 +0300

    Refactor API_Worker_Functions.ts and api-workers.ts to enhance token management and streamline API request handling; implement mutex for thread safety in header retrieval and update variable naming for clarity.

--- src/utils/api-workers.ts
+++ src/utils/api-workers.ts
@@ -1,36 +1,33 @@
 import { request } from "@playwright/test";
 import dotenv from "dotenv";
 import path from "path";
-import { CrossNumberApiProduct, CrossNumbersYV_Pair, OutputManufacturer, OutputModelSeries, ProductCompatibilityResult, ProductReference } from "./Types";
-import { delay, getManufacturerCodes, getmodelCodes, getTargets, getToken, getEncryptedSearchCode } from "./API_Worker_Functions";
+import { CrossNumberApiProduct, OutputManufacturer, OutputModelSeries, ProductCompatibilityResult, ProductReference } from "./Types";
+import { delay, getManufacturerCodes, getmodelCodes, getTargets, getEncryptedSearchCode, getAuthHeaders } from "./API_Worker_Functions";
 import { writeToFileIfNotExistsProducts } from "./TextUtils";
-import { getGroupNumberOfProductType } from "./Variables";
+import { productGroupNumbersOfRepxpert } from "./Variables";
 
 dotenv.config({ path: path.resolve(".env") });
 
 const productType = process.env.PRODUCT_TYPE as string;
 
 
-
 export async function processProductFor_OE(element: ProductReference): Promise<any> {
 
-  const { yvNo, supplier: filterBrand, crossNumber } = element;
-  const token = await getToken();
+  const { yvNo, supplier, crossNumber } = element;
 
   if (!crossNumber) {
-    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${filterBrand}`);
+    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${supplier}`);
     return null;
   }
 
-  console.log(`Processing YV: ${yvNo}, Brand: ${filterBrand}, Cross Number: ${crossNumber}`);
-
+  console.log(`Processing YV: ${yvNo}, Brand: ${supplier}, Cross Number: ${crossNumber}`);
   const apiContext = await request.newContext();
 
   try {
     // 1️⃣ Encrypted Search Code alma
     const encryptedSearchCode = await getEncryptedSearchCode(
       crossNumber,
-      filterBrand,
+      supplier,
       apiContext,
     );
 
@@ -38,19 +35,15 @@ export async function processProductFor_OE(element: ProductReference): Promise<a
     const oeru_1 = process.env.OE_REQUEST_URL_1 as string;
     const oeru_2 = process.env.OE_REQUEST_URL_2 as string;
     const oeURL = `${oeru_1}${encryptedSearchCode}${oeru_2}`;
-
     //console.log(oeURL);
 
-    const oeResp = await apiContext.get(oeURL, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-
+    const oeResp = await apiContext.get(oeURL, { headers: await getAuthHeaders() });
     const oeData = await oeResp.json();
 
     const result: any = {
       yvNo,
       crossNumber,
-      supplier: filterBrand,
+      supplier: supplier,
       oeNumbers: [],
     };
 
@@ -75,29 +68,23 @@ export async function processProductFor_OE(element: ProductReference): Promise<a
 export async function processProductFor_VehicleCompatibility(element: ProductReference): Promise<ProductCompatibilityResult | null> {
 
   const apiContext = await request.newContext();
-  const { yvNo, supplier: filterBrand, crossNumber } = element;
-  const token = await getToken();
-
-  if (!token) {
-    console.error(`Failed to get token for YV: ${yvNo}, Brand: ${filterBrand}`);
-    return null;
-  }
+  const { yvNo, supplier, crossNumber } = element;
 
   if (!crossNumber) {
-    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${filterBrand}`);
+    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${supplier}`);
     return null;
   }
 
   // Çıktı için ana nesne yapısı
-  const result: ProductCompatibilityResult = { yvNo, crossNumber, brand: filterBrand, compatibleVehicles: [] };
+  const result: ProductCompatibilityResult = { yvNo, crossNumber, brand: supplier, compatibleVehicles: [] };
 
   try {
     // 1️⃣ Encrypted Search Code alma
-    const encryptedSearchCode = await getEncryptedSearchCode(crossNumber, filterBrand, apiContext);
+    const encryptedSearchCode = await getEncryptedSearchCode(crossNumber, supplier, apiContext);
 
     if (!encryptedSearchCode) {
-      console.warn(`No encrypted search code found for ${crossNumber} - YV: ${yvNo}, Brand: ${filterBrand} - Product couldn't be found !!!`);
-      await writeToFileIfNotExistsProducts(`YV: ${yvNo}, Brand: ${filterBrand} -  ${crossNumber}`);
+      console.warn(`No encrypted search code found for ${crossNumber} - YV: ${yvNo}, Brand: ${supplier} - Product couldn't be found !!!`);
+      await writeToFileIfNotExistsProducts(`YV: ${yvNo}, Brand: ${supplier} -  ${crossNumber}`);
       return null;
     }
 
@@ -144,7 +131,7 @@ export async function processProductFor_VehicleCompatibility(element: ProductRef
     }
 
     if (result.compatibleVehicles.length === 0) {
-      console.warn(`No compatible vehicles found for YV: ${yvNo}, Brand: ${filterBrand}, Cross Number: ${crossNumber}`);
+      console.warn(`No compatible vehicles found for YV: ${yvNo}, Brand: ${supplier}, Cross Number: ${crossNumber}`);
     }
     return result;
   } catch (err) {
@@ -159,36 +146,29 @@ export async function processProductFor_VehicleCompatibility(element: ProductRef
 
 export async function processProductFor_CrossNumbers(element: ProductReference) {
   const apiContext = await request.newContext();
-  const { yvNo, supplier: filterBrand, crossNumber } = element;
-  console.log(`Processing YV: ${yvNo}, Brand: ${filterBrand}, Cross Number: ${crossNumber}`);
-  const token = await getToken();
-  //console.log(`Token: `, token);
+  const { yvNo, supplier, crossNumber } = element;
 
-  if (!token) {
-    console.error(`Failed to get token for YV: ${yvNo}, Brand: ${filterBrand}`);
-    return null;
-  }
+  console.log(`Processing YV: ${yvNo}, Brand: ${supplier}, Cross Number: ${crossNumber}`);
 
   if (!crossNumber) {
-    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${filterBrand}`);
+    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${supplier}`);
     return null;
   }
 
-  const crossNumberURL_1 = process.env.CROSS_NUMBER_URL_1 || "";
-  const crossNumberURL_2 = process.env.CROSS_NUMBER_URL_2 || "";
-  const crossNumberURL_3 = process.env.CROSS_NUMBER_URL_3 || "";
-  const queryNumber = element.crossNumber;                       // The number or code will be searched
+  const part_1 = process.env.CROSS_NUMBER_URL_1 || "";
+  const part_2 = process.env.CROSS_NUMBER_URL_2 || "";
+  const part_3 = process.env.CROSS_NUMBER_URL_3 || "";
+  const queryCode = element.crossNumber;                       // The number or code will be searched
   const querySize = "200";                                       // Default size is 20 but we are extending it to 200 to get all cross numbers from all suppliers
-  const groupNumber = getGroupNumberOfProductType(productType);  // Each product type has a unique grroup number for API requests
+  const groupNumber = productGroupNumbersOfRepxpert[productType]; // Each product type has a unique grroup number for API requests
 
-  const crossNumberURL = `${crossNumberURL_1}${queryNumber}${crossNumberURL_2}${groupNumber}${crossNumberURL_3}${querySize}`;
+  const crossNumberURL = `${part_1}${queryCode}${part_2}${groupNumber}${part_3}${querySize}`;
   //console.log(`Cross Number URL: ${crossNumberURL}`);
 
-  const response = await apiContext.get(crossNumberURL, { headers: { Authorization: `Bearer ${token}` } });
+  const response = await apiContext.get(crossNumberURL, { headers: await getAuthHeaders() });
   const data = await response.json();
   const products: any[] = data.products;
 
-  const result: CrossNumbersYV_Pair[] = [];
   const targets: CrossNumberApiProduct[] = [];
 
   for (const product of products) {
@@ -206,56 +186,47 @@ export async function processProductFor_CrossNumbers(element: ProductReference) 
     OE: element.crossNumber,
     crossNumbers: targets
   }
-
 }
-
 
 export async function processProductForArticleAttributes(element: ProductReference) {
 
-  const { yvNo, supplier: filterBrand, crossNumber } = element;
-  const token = await getToken();
+  const { yvNo, supplier, crossNumber } = element;
 
   if (!crossNumber) {
-    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${filterBrand}`);
+    console.warn(`No cross number found for YV: ${yvNo}, Brand: ${supplier}`);
     return null;
   }
 
-  console.log(`Processing YV: ${yvNo}, Brand: ${filterBrand}, Cross Number: ${crossNumber}`);
+  console.log(`Processing YV: ${yvNo}, Brand: ${supplier}, Cross Number: ${crossNumber}`);
 
   const apiContext = await request.newContext();
 
   try {
-
     // 1️⃣ Encrypted Search Code alma
-    const encryptedSearchCode = await getEncryptedSearchCode(
-      crossNumber,
-      filterBrand,
-      apiContext,
-    );
+    const encryptedSearchCode = await getEncryptedSearchCode( crossNumber, supplier, apiContext);
 
     // 2️⃣ Article Attributes alma
+    const part_1 = process.env.ARTICLE_ATTRIBUTES_URL_1 as string;
+    const part_2 = process.env.ARTICLE_ATTRIBUTES_URL_2 as string;
+    const URL = `${part_1}${encryptedSearchCode}${part_2}`;
 
-    const attribute_url_1 = process.env.ARTICLE_ATTRIBUTES_URL_1 as string;
-    const attribute_url_2 = process.env.ARTICLE_ATTRIBUTES_URL_2 as string;
-    const URL = `${attribute_url_1}${encryptedSearchCode}${attribute_url_2}`;
-
-    const response = await apiContext.get(URL, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-
-    const data = await response.json();
+    const headers = await getAuthHeaders();
+    console.log(headers);
+    const response = await apiContext.get(URL, { headers: await getAuthHeaders() });
 
     const result: any = {
       yvNo,
       crossNumber,
-      supplier: filterBrand,
+      supplier: supplier,
       attributes: [],
     };
 
-    for (const element of data.classifications[0].features) {
-      const values = Object.values(element.featureValues[0]).join(', ');
-      //console.log(`${element.name} - ${values}`);
+    const data = await response.json();
 
+    for (const element of data.classifications[0].features) {
+      
+      const values = Object.values(element.featureValues[0]).join(', ');
+      
       result.attributes.push({
         name: element.name,
         value: values
@@ -263,7 +234,7 @@ export async function processProductForArticleAttributes(element: ProductReferen
     }
 
     return result;
-    
+
   } catch (err) {
     console.error(`Error for YV ${yvNo} : ${crossNumber}: ${err}`);
     return null;
@@ -271,5 +242,4 @@ export async function processProductForArticleAttributes(element: ProductReferen
     await apiContext.dispose();
     await delay(300);
   }
-
 }
