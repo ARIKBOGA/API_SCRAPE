405e50504c3041ad13ca8aa79a029eccb13e024e
ARIKBOGA <barikboga42@gmail.com>
2025-08-09 01:44:08 +0300

    Add writeToExcel function to generate Excel files from data

--- src/Producers/EAC/TS/EAC_helpers.ts
+++ src/Producers/EAC/TS/EAC_helpers.ts
@@ -1,6 +1,7 @@
 import { AlignmentType, Document, Packer, Paragraph, TextRun } from "docx";
 import * as path from "path";
 import * as fs from "fs";
+import XLSX from "xlsx";
 
 export function createDocxDocument(...values: string[]) {
     const sections = values.map((value) => ({
@@ -30,3 +31,31 @@ export function writeToWord(documents: {document: Document; filename: string;}[]
         });
     })
 }
+
+
+export function writeToExcel(EXCEL_Sheets: { fileName: string; numbers: string[]; columnCount: number }[]) {
+
+  const OUTPUT_FILE_PATH = path.resolve(__dirname, `../data/documents/excels/Full_EAC_Numbers.xlsx`);
+  
+  const wb = XLSX.utils.book_new();
+
+  // Iterate over the data array and create a worksheet for each object
+  EXCEL_Sheets.forEach((element) => {
+    const { fileName, numbers, columnCount } = element;
+    const sheet_data: string[][] = [];
+
+    // Iterate over the numbers array and create chunks of columnCount length
+    for (let i = 0; i < numbers.length; i += columnCount) {
+      sheet_data.push(numbers.slice(i, i + columnCount)); // [1, columnCount], [columnCount+1, 2*columnCount], ...
+    }
+
+    // Create the worksheet from the worksheet data
+    const ws = XLSX.utils.aoa_to_sheet(sheet_data);
+    XLSX.utils.book_append_sheet(wb, ws, fileName);
+  });
+
+  XLSX.writeFile(wb, OUTPUT_FILE_PATH);
+
+  console.log(`✨ Excel dosyası oluşturuldu: ${OUTPUT_FILE_PATH}`);
+}
+
