e99fc08e148c3fd6716fc98f495ec1a7b125ed7a
YAVUZSAN <bilisim@yavuzsan.com>
2025-09-15 17:07:41 +0300

    Update scraping limits and add new data attributes
    Adjusts product scraping range by modifying start and end values
    to enhance data collection efficiency. Introduces JSON files
    providing detailed product attributes, cross-numbers, OE numbers,
    and vehicle compatibility for better data organization and lookup.
    
    These updates support improved integration with external systems
    and expand the dataset for enhanced product coverage.

--- src/Scrapers/API/Repxpert/API_Scrapers.spec.ts
+++ src/Scrapers/API/Repxpert/API_Scrapers.spec.ts
@@ -12,8 +12,8 @@ dotenv.config({ path: path.resolve(".env") });
 const productType = process.env.PRODUCT_TYPE as string;
 const filterBrand = (process.env.FILTER_BRAND as string) !== "" ? process.env.FILTER_BRAND as string : "LOADED_NOT_FOUND_COMMERCIAL_REMINDER";
 
-const start = 0;
-const end: number = 150;
+const start = 550;
+const end: number = 0;
 const end_str = end !== 0 ? end : "end";
 
 async function processProducts(processFunction: Function, fileName: string, threadLimit: number, processFor: string) {
